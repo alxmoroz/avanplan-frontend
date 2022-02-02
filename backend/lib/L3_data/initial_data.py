fad33174c99b897b5a3a2ac585adbaffd82060fb
Alexandr Moroz <alexandrmoroz@gmail.com>
2022-02-02 06:31:12 +0300

    #1936 шаблон проекта FastAPI

--- backend/lib/L3_data/initial_data.py
+++ backend/lib/L3_data/initial_data.py
@@ -1,7 +1,7 @@
 import logging
 
-from app_1.db.init_db import init_db
-from app_1.db.session import SessionLocal
+from .db.init_db import init_db
+from .db.session import SessionLocal
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)

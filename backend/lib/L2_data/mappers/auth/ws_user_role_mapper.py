1eff3acbf4abdb152c52ff68e7a0aa621ca00b6f
Alexandr Moroz <alexandrmoroz@gmail.com>
2022-04-28 01:42:14 +0300

    #2063 поправил ошибку в связующей таблице РП - пользователь - роль

--- backend/lib/L2_data/mappers/auth/ws_user_role_mapper.py
+++ backend/lib/L2_data/mappers/auth/ws_user_role_mapper.py
@@ -4,6 +4,9 @@
 from lib.L1_domain.entities import WSUserRole
 from lib.L2_data.models.auth import WSUserRole as WSUserRoleModel
 from lib.L2_data.schema import WSUserRoleSchemaGet, WSUserRoleSchemaUpsert
+from .workspace_mapper import WorkspaceMapper
+from .user_mapper import UserMapper
+from .ws_role_mapper import WSRoleMapper
 
 from ..base_mapper import BaseMapper
 
@@ -11,3 +14,11 @@ from ..base_mapper import BaseMapper
 class WSUserRoleMapper(BaseMapper[WSUserRoleSchemaGet, WSUserRoleSchemaUpsert, WSUserRole, WSUserRoleModel]):
     def __init__(self):
         super().__init__(schema_get_cls=WSUserRoleSchemaGet, schema_upd_cls=WSUserRoleSchemaUpsert, entity_cls=WSUserRole)
+
+    def entity_from_schema_get(self, s: WSUserRoleSchemaGet) -> WSUserRole | None:
+        if s:
+            r: WSUserRole = super().entity_from_schema_get(s)
+            r.user = UserMapper().entity_from_schema_get(s.user)
+            r.ws_role = WSRoleMapper().entity_from_schema_get(s.ws_role)
+            r.workspace = WorkspaceMapper().entity_from_schema_get(s.workspace)
+            return r
--- backend/lib/L2_data/models/auth/ws_user_role.py
+++ backend/lib/L2_data/models/auth/ws_user_role.py
@@ -1,6 +1,7 @@
 #  Copyright (c) 2022. Alexandr Moroz
 
 from sqlalchemy import Column, ForeignKey, Integer
+from sqlalchemy.orm import relationship
 
 from ..base_model import BaseModel
 
@@ -8,5 +9,10 @@ from ..base_model import BaseModel
 class WSUserRole(BaseModel):
 
     workspace_id = Column(Integer, ForeignKey("workspaces.id", ondelete="CASCADE"))
+    workspace = relationship("Workspace")
+
     user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"))
+    user = relationship("User")
+
     ws_role_id = Column(Integer, ForeignKey("wsroles.id", ondelete="CASCADE"))
+    ws_role = relationship("WSRole")

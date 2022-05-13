d09bbc725a17d091950d537deac4d46c5f41b50e
Alexandr Moroz <alexandrmoroz@gmail.com>
2022-05-13 03:00:43 +0300

    #2104 подготовка к публикации веб-версии

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,11 +4,12 @@
 
 flutter clean
 
+bash ./scripts/api_generate.sh
 bash ./scripts/build_runner_clean.sh
 bash ./scripts/build_runner_build.sh
 bash ./scripts/test.sh || exit
 bash ./scripts/bump_version.sh
 #bash ./scripts/build_android.sh
 #bash ./scripts/build_ios.sh
-#bash ./scripts/build_web.sh
+bash ./scripts/build_web.sh
 #bash ./deploy.sh

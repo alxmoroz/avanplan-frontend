bb74fa574996f0dbdc207cfadbcf60586aeaede4
Alexandr Moroz <alexandrmoroz@gmail.com>
2022-11-11 09:46:51 +0300

    #2219 лишний обсервер

--- lib/L3_app/views/sign_in/sign_in_view.dart
+++ lib/L3_app/views/sign_in/sign_in_view.dart
@@ -1,7 +1,6 @@
 // Copyright (c) 2022. Alexandr Moroz
 
 import 'package:flutter/cupertino.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 
 import '../../../L2_data/repositories/platform.dart';
@@ -20,49 +19,47 @@ class SignInView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Observer(
-      builder: (_) => MTPage(
-        body: SafeArea(
-          child: Center(
-            child: Container(
-              constraints: const BoxConstraints(maxWidth: SCR_S_WIDTH),
-              child: LayoutBuilder(
-                builder: (_, size) => ListView(
-                  shrinkWrap: true,
-                  children: [
-                    gerculesIcon(size: size.maxHeight / 7),
-                    H1(loc.app_title, align: TextAlign.center, padding: const EdgeInsets.only(bottom: P)),
+    return MTPage(
+      body: SafeArea(
+        child: Center(
+          child: Container(
+            constraints: const BoxConstraints(maxWidth: SCR_S_WIDTH),
+            child: LayoutBuilder(
+              builder: (_, size) => ListView(
+                shrinkWrap: true,
+                children: [
+                  gerculesIcon(size: size.maxHeight / 7),
+                  H1(loc.app_title, align: TextAlign.center, padding: const EdgeInsets.only(bottom: P)),
 
-                    MTButton.outlined(
-                      margin: const EdgeInsets.symmetric(horizontal: P).copyWith(top: P2),
-                      leading: googleIcon(size: MIN_BTN_HEIGHT),
-                      middle: MediumText(loc.auth_sign_in_with_google_btn_title, color: CupertinoColors.label),
-                      color: googleBtnColor,
-                      onTap: () => authController.signInWithGoogle(context),
-                    ),
-                    // для Андроида не показываем SignInWithApple
-                    if (!isAndroid && authController.signInWithAppleIsAvailable)
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: P).copyWith(top: P),
-                        child: SignInWithAppleButton(
-                          height: MIN_BTN_HEIGHT,
-                          text: loc.auth_sign_in_with_apple_btn_title,
-                          borderRadius: const BorderRadius.all(Radius.circular(DEF_BORDER_RADIUS)),
-                          style: MediaQuery.of(context).platformBrightness == Brightness.dark
-                              ? SignInWithAppleButtonStyle.white
-                              : SignInWithAppleButtonStyle.black,
-                          onPressed: () => authController.signInWithApple(context),
-                        ),
+                  MTButton.outlined(
+                    margin: const EdgeInsets.symmetric(horizontal: P).copyWith(top: P2),
+                    leading: googleIcon(size: MIN_BTN_HEIGHT),
+                    middle: MediumText(loc.auth_sign_in_with_google_btn_title, color: CupertinoColors.label),
+                    color: googleBtnColor,
+                    onTap: () => authController.signInWithGoogle(context),
+                  ),
+                  // для Андроида не показываем SignInWithApple
+                  if (!isAndroid && authController.signInWithAppleIsAvailable)
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: P).copyWith(top: P),
+                      child: SignInWithAppleButton(
+                        height: MIN_BTN_HEIGHT,
+                        text: loc.auth_sign_in_with_apple_btn_title,
+                        borderRadius: const BorderRadius.all(Radius.circular(DEF_BORDER_RADIUS)),
+                        style: MediaQuery.of(context).platformBrightness == Brightness.dark
+                            ? SignInWithAppleButtonStyle.white
+                            : SignInWithAppleButtonStyle.black,
+                        onPressed: () => authController.signInWithApple(context),
                       ),
-                    MTButton.outlined(
-                      margin: const EdgeInsets.symmetric(horizontal: P).copyWith(top: P),
-                      titleText: loc.auth_show_sign_in_form_action_title,
-                      onTap: () => showSignInPasswordDialog(context),
                     ),
-                    const SizedBox(height: P * 3),
-                    const SignInTermsLinks(),
-                  ],
-                ),
+                  MTButton.outlined(
+                    margin: const EdgeInsets.symmetric(horizontal: P).copyWith(top: P),
+                    titleText: loc.auth_show_sign_in_form_action_title,
+                    onTap: () => showSignInPasswordDialog(context),
+                  ),
+                  const SizedBox(height: P * 3),
+                  const SignInTermsLinks(),
+                ],
               ),
             ),
           ),

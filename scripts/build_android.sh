2bec73443388c1169b2da83a628ca3b11cc5acbb
Alexandr Moroz <alexandrmoroz@gmail.com>
2023-01-17 13:19:31 +0300

    #2368 скрипт сборки для андроида

--- scripts/build_android.sh
+++ scripts/build_android.sh
@@ -5,6 +5,7 @@
 echo "BUILDING FOR ANDROID..."
 
 #flutter build apk --release -t lib/L3_app/main.dart
-flutter build appbundle --target-platform android-arm,android-arm64,android-x64 --release
+#flutter build appbundle --target-platform android-arm,android-arm64,android-x64 --release
+flutter build appbundle --release
 
 echo "BUILDING FOR ANDROID COMPLETE"

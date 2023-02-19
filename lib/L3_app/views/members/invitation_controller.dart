0ba781611adcb1d20f78ca1aca556a90e02f7cff
Alexandr Moroz <alexandrmoroz@gmail.com>
2023-02-19 16:49:29 +0300

    #2525 отображение ссылки для веба и кнопка "скопировать"

--- lib/L3_app/views/members/invitation_controller.dart
+++ lib/L3_app/views/members/invitation_controller.dart
@@ -1,12 +1,17 @@
 // Copyright (c) 2023. Alexandr Moroz
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:mobx/mobx.dart';
+import 'package:share_plus/share_plus.dart';
 
 import '../../../../L1_domain/entities/invitation.dart';
 import '../../../../L1_domain/entities/role.dart';
 import '../../../../L1_domain/entities/task.dart';
+import '../../../L2_data/services/platform.dart';
+import '../../components/colors.dart';
 import '../../components/text_field_annotation.dart';
+import '../../components/text_widgets.dart';
 import '../../extra/services.dart';
 import '../../presenters/date_presenter.dart';
 import '../_base/edit_controller.dart';
@@ -79,4 +84,30 @@ abstract class _InvitationControllerBase extends EditController with Store {
   }
 
   String get invitationSubject => '${loc.invitation_share_subject_prefix}${loc.app_title} - ${task.title}';
+
+  Future shareInvitation(BuildContext context) async {
+    if (invitationUrl.isEmpty) {
+      await createInvitation();
+    }
+
+    if (!isWeb) {
+      final box = context.findRenderObject() as RenderBox?;
+      await Share.shareWithResult(
+        invitationUrl,
+        subject: invitationSubject,
+        sharePositionOrigin: box!.localToGlobal(Offset.zero) & box.size,
+      );
+    }
+  }
+
+  void copyUrl(BuildContext context) {
+    Clipboard.setData(ClipboardData(text: invitationUrl));
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: NormalText(loc.copied_notification_title, align: TextAlign.center),
+        backgroundColor: darkBackgroundColor.resolve(context),
+        duration: const Duration(milliseconds: 1234),
+      ),
+    );
+  }
 }
--- lib/L3_app/views/members/invitation_pane.dart
+++ lib/L3_app/views/members/invitation_pane.dart
@@ -2,7 +2,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
-import 'package:share_plus/share_plus.dart';
 
 import '../../../L2_data/services/platform.dart';
 import '../../components/colors.dart';
@@ -10,7 +9,6 @@ import '../../components/constants.dart';
 import '../../components/icons.dart';
 import '../../components/mt_button.dart';
 import '../../components/mt_text_field.dart';
-import '../../components/text_widgets.dart';
 import '../../extra/services.dart';
 import 'invitation_controller.dart';
 
@@ -40,40 +38,18 @@ class InvitationPane extends StatelessWidget {
           );
   }
 
-  Future _shareInvitation(BuildContext context) async {
-    if (!_hasUrl) {
-      await controller.createInvitation();
-    }
-
-    if (!isWeb) {
-      final box = context.findRenderObject() as RenderBox?;
-      await Share.shareWithResult(
-        controller.invitationUrl,
-        subject: controller.invitationSubject,
-        sharePositionOrigin: box!.localToGlobal(Offset.zero) & box.size,
-      );
-    }
-  }
-
   bool get _canShare => _hasUrl || controller.validated;
 
   Widget _copyButton(BuildContext context) => MTButton(
-        middle: Row(
-          children: [
-            Container(height: P * 3, width: 1, color: borderColor.resolve(context)),
-            const SizedBox(width: P),
-            const CopyIcon(),
-            const SizedBox(width: P),
-          ],
-        ),
-        onTap: () => ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content: NormalText(loc.copied_notification_title, align: TextAlign.center),
-            backgroundColor: darkBackgroundColor.resolve(context),
-            duration: const Duration(milliseconds: 1234),
-          ),
-        ),
-      );
+      middle: Row(
+        children: [
+          Container(height: P * 3, width: 1, color: borderColor.resolve(context)),
+          const SizedBox(width: P),
+          const CopyIcon(),
+          const SizedBox(width: P),
+        ],
+      ),
+      onTap: () => controller.copyUrl(context));
 
   @override
   Widget build(BuildContext context) => Observer(
@@ -90,7 +66,7 @@ class InvitationPane extends StatelessWidget {
                     leading: ShareIcon(size: P2, color: _canShare ? mainColor : lightGreyColor),
                     constrained: false,
                     titleText: loc.invitation_share_action_title,
-                    onTap: _canShare ? () => _shareInvitation(context) : null,
+                    onTap: _canShare ? () => controller.shareInvitation(context) : null,
                     padding: const EdgeInsets.symmetric(horizontal: P2),
                     margin: const EdgeInsets.only(top: P2),
                   ),

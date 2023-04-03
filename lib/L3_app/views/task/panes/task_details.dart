47f4475a27188e9dfa622edc82847e16873ecdfc
Alexandr Moroz <alexandrmoroz@gmail.com>
2023-04-03 08:21:01 +0300

    #2149 Добавить возможность обработки ссылок #2306 Выбор текста

--- lib/L3_app/views/task/panes/task_details.dart
+++ lib/L3_app/views/task/panes/task_details.dart
@@ -1,9 +1,12 @@
 // Copyright (c) 2022. Alexandr Moroz
 
 import 'package:flutter/material.dart';
+import 'package:flutter_linkify/flutter_linkify.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 import '../../../../L1_domain/entities/task.dart';
 import '../../../../L1_domain/entities_extensions/task_members.dart';
+import '../../../components/colors.dart';
 import '../../../components/constants.dart';
 import '../../../components/text_widgets.dart';
 import '../../../presenters/person_presenter.dart';
@@ -15,7 +18,12 @@ class TaskDetails extends StatelessWidget {
   bool get hasDescription => task.description.isNotEmpty;
   bool get hasAuthor => task.author != null;
 
-  Widget description() => LightText(task.description, maxLines: 1000);
+  Widget description(BuildContext context) => SelectableLinkify(
+        text: task.description,
+        style: const LightText('').style(context),
+        linkStyle: const NormalText('', color: mainColor).style(context),
+        onOpen: (link) async => await launchUrlString(link.url),
+      );
 
   Widget? get bottomBar => null;
 
@@ -25,7 +33,7 @@ class TaskDetails extends StatelessWidget {
     return ListView(
       padding: padding.add(const EdgeInsets.all(P).copyWith(bottom: padding.bottom > 0 ? 0 : P)),
       children: [
-        if (hasDescription) description(),
+        if (hasDescription) description(context),
         if (hasAuthor) ...[
           const SizedBox(height: P_2),
           Row(children: [const Spacer(), task.author!.iconName()]),

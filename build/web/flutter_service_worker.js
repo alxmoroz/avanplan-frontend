4487dc9c6239103a4d182aabd81154bb0c4eb1aa
Alexandr Moroz <alexandrmoroz@gmail.com>
2023-06-25 18:11:44 +0300

    Bump version to 1.3.1404

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"version.json": "282c16d0a26d637ab4efabadf58859f6",
+const RESOURCES = {"version.json": "544ebe43e7467cfd729b0e1125564919",
 "splash/img/light-2x.png": "722daceadc705fefd3d7a1a2959a62e2",
 "splash/img/dark-4x.png": "76c073051c5e7210c673640f8584523d",
 "splash/img/light-3x.png": "12e2006ccbec99536729da5c2ac489bb",
@@ -14,9 +14,9 @@ const RESOURCES = {"version.json": "282c16d0a26d637ab4efabadf58859f6",
 "splash/img/light-1x.png": "94ba8fa14ea681cdb77dfb8cc85f8c02",
 "splash/splash.js": "123c400b58bea74c1305ca3ac966748d",
 "splash/style.css": "b0640fc6c03fe3a7e6d2f1d2de02349c",
-"index.html": "92fb585deda393ad85a8c28102a249d5",
-"/": "92fb585deda393ad85a8c28102a249d5",
-"main.dart.js": "efd135fbb79975d9e2a4ddd752211072",
+"index.html": "3ae571e52447dda2a4cc50644de574ce",
+"/": "3ae571e52447dda2a4cc50644de574ce",
+"main.dart.js": "9b2b16ea1e8c775f6c8c2a80bc87e728",
 ".well-known/apple-app-site-association": "bda08a6d5ec9db358dab1e697df75298",
 ".well-known/assetlinks.json": "768c5dec63b05d4c684582be06574c1c",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",

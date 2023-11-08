a05629a0b628502215fc8cb384885b13e1f6991f
Alexandr Moroz <alexandrmoroz@gmail.com>
2023-11-08 14:34:38 +0300

    ошибка добавления и редактирования комментариев, названия и описания проекта

--- lib/L3_app/usecases/ws_statuses.dart
+++ lib/L3_app/usecases/ws_statuses.dart
@@ -6,6 +6,7 @@ import '../extra/services.dart';
 
 extension WSStatusesUC on Workspace {
   Iterable<Status> get statuses => statusesController.statuses(id!);
-  Iterable<Status> get defaultStatuses => statuses.where((st) => st.allProjects);
+  Iterable<Status> get _allProjectStatuses => statuses.where((st) => st.allProjects);
+  Iterable<Status> get defaultStatuses => _allProjectStatuses.isNotEmpty ? _allProjectStatuses : statuses;
   Status? status(int statusId) => statusesController.status(id!, statusId);
 }

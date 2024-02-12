bd0068c1a49a6431fd5882d40fd5976302c06ec6
Alexandr Moroz <alexandrmoroz@gmail.com>
2024-02-13 01:26:06 +0300

    Опции и чек для тарифов - вынес отдельный файл для карточки тарифа

---
+++ lib/L3_app/views/tariff/tariff_card.dart
@@ -0,0 +1,58 @@
+// Copyright (c) 2024. Alexandr Moroz
+
+import 'package:flutter/cupertino.dart';
+
+import '../../../L1_domain/entities/tariff.dart';
+import '../../components/button.dart';
+import '../../components/card.dart';
+import '../../components/colors_base.dart';
+import '../../components/constants.dart';
+import '../../components/icons.dart';
+import '../../components/text.dart';
+import '../../extra/services.dart';
+import '../../presenters/tariff.dart';
+import '../../usecases/ws_actions.dart';
+import 'tariff_base_price.dart';
+import 'tariff_limits.dart';
+import 'tariff_selector_controller.dart';
+
+class TariffCard extends StatelessWidget {
+  const TariffCard(this._tariff, this._controller, this._isCurrent, {super.key});
+
+  final Tariff _tariff;
+  final bool _isCurrent;
+  final TariffSelectorController _controller;
+
+  Widget _selectButton(BuildContext context, Tariff tariff) => MTButton.main(
+        titleText: loc.tariff_select_action_title,
+        margin: const EdgeInsets.symmetric(horizontal: P3),
+        onTap: () => _controller.selectTariff(context, tariff),
+      );
+
+  @override
+  Widget build(BuildContext context) {
+    final smallHeight = MediaQuery.sizeOf(context).height < SCR_XS_HEIGHT;
+
+    return MTCard(
+      margin: const EdgeInsets.symmetric(horizontal: P2).copyWith(bottom: MediaQuery.paddingOf(context).bottom == 0 ? P3 : P),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          H2(_tariff.title, align: TextAlign.center, padding: const EdgeInsets.all(P3)),
+          if (smallHeight) const Spacer() else Expanded(child: TariffLimits(_tariff)),
+          TariffBasePrice(_tariff),
+          const SizedBox(height: P3),
+          _isCurrent
+              ? MTButton.main(
+                  titleText: loc.tariff_current_title,
+                  margin: const EdgeInsets.symmetric(horizontal: P3),
+                )
+              : _controller.ws.hpTariffUpdate
+                  ? _selectButton(context, _tariff)
+                  : const MTButton.main(middle: PrivacyIcon(color: f2Color), margin: EdgeInsets.symmetric(horizontal: P3)),
+          const SizedBox(height: P3),
+        ],
+      ),
+    );
+  }
+}
--- lib/L3_app/views/tariff/tariff_selector.dart
+++ lib/L3_app/views/tariff/tariff_selector.dart
@@ -6,7 +6,6 @@ import 'package:flutter_mobx/flutter_mobx.dart';
 import '../../../L1_domain/entities/tariff.dart';
 import '../../../L2_data/services/platform.dart';
 import '../../components/button.dart';
-import '../../components/card.dart';
 import '../../components/circular_progress.dart';
 import '../../components/colors.dart';
 import '../../components/colors_base.dart';
@@ -16,11 +15,8 @@ import '../../components/icons.dart';
 import '../../components/text.dart';
 import '../../components/toolbar.dart';
 import '../../extra/services.dart';
-import '../../presenters/tariff.dart';
-import '../../usecases/ws_actions.dart';
 import 'request_tariff_card.dart';
-import 'tariff_base_price.dart';
-import 'tariff_limits.dart';
+import 'tariff_card.dart';
 import 'tariff_selector_controller.dart';
 
 Future<Tariff?> selectTariff(int wsId, {String reason = ''}) async {
@@ -36,38 +32,11 @@ class _TariffSelectorDialog extends StatelessWidget {
   const _TariffSelectorDialog(this._controller);
   final TariffSelectorController _controller;
 
-  Widget _selectButton(BuildContext context, Tariff tariff) => MTButton.main(
-        titleText: loc.tariff_select_action_title,
-        margin: const EdgeInsets.symmetric(horizontal: P3),
-        onTap: () => _controller.selectTariff(context, tariff),
-      );
-
   Widget _tariffCard(BuildContext context, int index) {
-    final smallHeight = MediaQuery.sizeOf(context).height < SCR_XS_HEIGHT;
     Widget? card;
     if (index < _controller.tariffs.length) {
       final tariff = _controller.tariffs.elementAt(index);
-      card = MTCard(
-        margin: const EdgeInsets.symmetric(horizontal: P2).copyWith(bottom: MediaQuery.paddingOf(context).bottom == 0 ? P3 : P),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            H2(tariff.title, align: TextAlign.center, padding: const EdgeInsets.all(P3)),
-            if (smallHeight) const Spacer() else Expanded(child: TariffLimits(tariff)),
-            TariffBasePrice(tariff),
-            const SizedBox(height: P3),
-            _controller.activeTariffIndex != index
-                ? _controller.ws.hpTariffUpdate
-                    ? _selectButton(context, tariff)
-                    : const MTButton.main(middle: PrivacyIcon(color: f2Color), margin: EdgeInsets.symmetric(horizontal: P3))
-                : MTButton.main(
-                    titleText: loc.tariff_current_title,
-                    margin: const EdgeInsets.symmetric(horizontal: P3),
-                  ),
-            const SizedBox(height: P3),
-          ],
-        ),
-      );
+      card = TariffCard(tariff, _controller, index == _controller.activeTariffIndex);
     } else {
       card = const RequestTariffCard();
     }

5492efa3582baf7f2af7d6469e74c25aff34f1b1
Alexandr Moroz <alexandrmoroz@gmail.com>
2024-02-13 03:27:17 +0300

    Опции и чек для тарифов - оформление опций

--- lib/L3_app/views/tariff/tariff_options.dart
+++ lib/L3_app/views/tariff/tariff_options.dart
@@ -3,8 +3,11 @@
 import 'package:flutter/material.dart';
 
 import '../../../L1_domain/entities/tariff.dart';
+import '../../components/adaptive.dart';
 import '../../components/colors.dart';
+import '../../components/colors_base.dart';
 import '../../components/constants.dart';
+import '../../components/currency.dart';
 import '../../components/icons.dart';
 import '../../components/icons_workspace.dart';
 import '../../components/text.dart';
@@ -23,19 +26,23 @@ class _TariffOption extends StatelessWidget {
 
   TariffOption? get _option => _tariff.optionsMap[_code];
   num get _freeLimit => _option?.freeLimit ?? 0;
-  int get _freeLimitHuman => (_freeLimit / (_option?.tariffQuantity ?? 1)).round();
+  num get _quantity => _option?.tariffQuantity ?? 1;
+  int get _freeLimitHuman => (_freeLimit / _quantity).round();
 
   @override
   Widget build(BuildContext context) {
     String unit = '';
     String suffix = '';
+    String quantityStr = '';
 
     Widget icon = const SizedBox(height: iconSize);
     if (_code == TOCode.FS_VOLUME) {
       icon = const FileStorageIcon(size: iconSize, color: iconColor);
       suffix = '${_freeLimit.humanBytesSuffix} ';
+      quantityStr = _quantity.humanBytesStr;
       unit = loc.tariff_option_fs_volume_suffix;
     } else {
+      quantityStr = _quantity.humanValueStr;
       if (_code == TOCode.USERS_COUNT) {
         icon = const PeopleIcon(size: iconSize, color: iconColor);
         unit = loc.member_plural(_freeLimit);
@@ -47,7 +54,7 @@ class _TariffOption extends StatelessWidget {
     }
 
     return SizedBox(
-      width: (SCR_XXS_WIDTH - P4) / 3,
+      width: (SCR_XXS_WIDTH - P6) / 3,
       child: Column(
         children: [
           icon,
@@ -55,6 +62,10 @@ class _TariffOption extends StatelessWidget {
           D3('$_freeLimitHuman'),
           const SizedBox(height: P),
           SmallText('$suffix$unit', maxLines: 1),
+          const SizedBox(height: P3),
+          D4('+$quantityStr'),
+          const SizedBox(height: P),
+          MTPrice(_option?.price ?? 0, color: f3Color, size: AdaptiveSize.xs),
         ],
       ),
     );
@@ -67,12 +78,29 @@ class TariffOptions extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
+    final bs = BorderSide(color: b1Color.resolve(context));
+    const br = Radius.circular(DEF_BORDER_RADIUS);
+    return Stack(
       children: [
-        _TariffOption(_tariff, TOCode.USERS_COUNT),
-        _TariffOption(_tariff, TOCode.TASKS_COUNT),
-        _TariffOption(_tariff, TOCode.FS_VOLUME),
+        Container(
+          margin: const EdgeInsets.symmetric(horizontal: P3).copyWith(top: P * 17),
+          height: P11,
+          decoration: BoxDecoration(
+            border: Border(left: bs, right: bs, bottom: bs),
+            borderRadius: const BorderRadius.only(bottomLeft: br, bottomRight: br),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: P2),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              _TariffOption(_tariff, TOCode.USERS_COUNT),
+              _TariffOption(_tariff, TOCode.TASKS_COUNT),
+              _TariffOption(_tariff, TOCode.FS_VOLUME),
+            ],
+          ),
+        ),
       ],
     );
   }

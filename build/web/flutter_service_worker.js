3192a2dccf65dca61e415439d740f7f66844a3c6
Alexandr Moroz <alexandrmoroz@gmail.com>
2024-05-16 11:43:30 +0300

    Bump version to 1.21.2296

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"version.json": "b9ad4f51fc73179cde7d52076bd6579d",
+const RESOURCES = {"version.json": "a642970808ef21333e95927da03657c4",
 "splash/img/light-2x.png": "de04581de96618c7774eb07917cbb0b5",
 "splash/img/dark-4x.png": "d23a48799930e68b79537f9521b54392",
 "splash/img/light-3x.png": "0bd86804e97e7c72cff21b1d4bc703ca",
@@ -12,10 +12,10 @@ const RESOURCES = {"version.json": "b9ad4f51fc73179cde7d52076bd6579d",
 "splash/img/dark-2x.png": "1de000216e9b5b35639dc772bdb23a54",
 "splash/img/dark-1x.png": "efade7303c0cbca00b2a44781e1f4732",
 "splash/img/light-1x.png": "de41a29d2394649c3b563d87703d1dcb",
-"index.html": "4984db4f27b22652a27700ba9fa83475",
-"/": "4984db4f27b22652a27700ba9fa83475",
+"index.html": "90338b0db525eed935953d7697d6dd7a",
+"/": "90338b0db525eed935953d7697d6dd7a",
 "firebase-messaging-sw.js": "28562d3c6999025f15c6faa4e861b175",
-"main.dart.js": "c40664182f86e37b97aabd2a3d7ae706",
+"main.dart.js": "baa7940c8f70d5e9d3e35720f349cd7c",
 ".well-known/apple-app-site-association": "bda08a6d5ec9db358dab1e697df75298",
 ".well-known/assetlinks.json": "768c5dec63b05d4c684582be06574c1c",
 "flutter.js": "c71a09214cb6f5f8996a531350400a9a",

0b79ca60c90c32ef49b45654ff992aa34c5cd8ff
Alexandr Moroz <alexandrmoroz@gmail.com>
2024-06-05 23:04:47 +0300

    Тактильный отклик для PTR

--- lib/L3_app/components/refresh.dart
+++ lib/L3_app/components/refresh.dart
@@ -2,9 +2,10 @@
 
 import 'package:flutter/material.dart';
 
+import 'button.dart';
 import 'constants.dart';
 
-class MTRefresh extends StatelessWidget {
+class MTRefresh extends StatelessWidget with FocusManaging {
   const MTRefresh({super.key, required this.onRefresh, required this.child});
 
   final Widget child;
@@ -13,7 +14,7 @@ class MTRefresh extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return RefreshIndicator(
-      onRefresh: onRefresh,
+      onRefresh: () => tapAction(context, true, onRefresh, fbType: FeedbackType.light),
       edgeOffset: MediaQuery.of(context).padding.top,
       displacement: P2,
       child: child,

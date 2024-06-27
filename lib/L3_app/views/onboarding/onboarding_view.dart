b9bc7f5f0fb2c3bb1b0461b8fe68df5f4540efdf
Alexandr Moroz <alexandrmoroz@gmail.com>
2024-06-27 10:07:13 +0300

    Оформление в онбординге

--- lib/L3_app/views/onboarding/onboarding_view.dart
+++ lib/L3_app/views/onboarding/onboarding_view.dart
@@ -1,16 +1,15 @@
 // Copyright (c) 2024. Alexandr Moroz
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:intl/intl.dart';
 
+import '../../components/adaptive.dart';
 import '../../components/button.dart';
 import '../../components/colors.dart';
 import '../../components/constants.dart';
 import '../../components/icons.dart';
 import '../../components/images.dart';
-import '../../components/list_tile.dart';
 import '../../components/page.dart';
 import '../../components/text.dart';
 import '../../extra/route.dart';
@@ -47,71 +46,71 @@ class _OnboardingView extends StatelessWidget {
     return Observer(builder: (_) {
       return MTPage(
         appBar: QuizHeader(_controller),
-        body: Center(
-          child: ListView(
-            shrinkWrap: true,
-            children: [
-              if (_controller.stepIndex < 3) ...[
-                MTImage(['done', 'milestone', 'devices_sync'][_controller.stepIndex]),
-                H2(
-                  Intl.message('onboarding_step_${_controller.stepIndex + 1}_title'),
-                  align: TextAlign.center,
-                  padding: const EdgeInsets.all(P6).copyWith(bottom: P3),
-                ),
-                BaseText(
-                  Intl.message('onboarding_step_${_controller.stepIndex + 1}_text'),
-                  align: TextAlign.center,
-                  padding: const EdgeInsets.symmetric(horizontal: P6),
-                ),
-                const SizedBox(height: P3),
-                QuizNextButton(_controller),
-              ],
-              if (_controller.isPromoFeaturesStep)
-                PromoFeatures(wsMainController.myWS, onLater: _controller.next)
-              else if (_controller.isWhereWeGoStep) ...[
-                H2(
-                  loc.onboarding_where_we_go_step_title,
-                  align: TextAlign.center,
-                  padding: const EdgeInsets.symmetric(horizontal: P6).copyWith(bottom: P2),
-                ),
-                MTCardButton(
-                  margin: const EdgeInsets.symmetric(horizontal: P4, vertical: P2),
-                  onTap: _startWithProject,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      MTListTile(
-                        leading: const ProjectsIcon(color: mainColor),
-                        middle: H3(loc.onboarding_start_with_project_title),
-                        trailing: const ChevronIcon(),
-                        padding: EdgeInsets.zero,
-                        bottomDivider: false,
-                      ),
-                      const SizedBox(height: P2),
-                      BaseText(loc.onboarding_start_with_project_text, align: TextAlign.left, maxLines: 3),
-                    ],
+        body: MTAdaptive.s(
+          child: Center(
+            child: ListView(
+              shrinkWrap: true,
+              children: [
+                if (_controller.stepIndex < 3) ...[
+                  MTImage(['done', 'milestone', 'devices_sync'][_controller.stepIndex]),
+                  H2(
+                    Intl.message('onboarding_step_${_controller.stepIndex + 1}_title'),
+                    align: TextAlign.center,
+                    padding: const EdgeInsets.all(P6).copyWith(bottom: P3),
                   ),
-                ),
-                MTCardButton(
-                  margin: const EdgeInsets.symmetric(horizontal: P4, vertical: P2),
-                  onTap: _startWithTasks,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      MTListTile(
-                        leading: const DoneIcon(true, size: P6),
-                        middle: H3(loc.onboarding_start_with_tasks_title),
-                        trailing: const ChevronIcon(),
-                        padding: EdgeInsets.zero,
-                        bottomDivider: false,
-                      ),
-                      const SizedBox(height: P2),
-                      BaseText(loc.onboarding_start_with_tasks_text, align: TextAlign.left, maxLines: 3),
-                    ],
+                  BaseText(
+                    Intl.message('onboarding_step_${_controller.stepIndex + 1}_text'),
+                    align: TextAlign.center,
+                    padding: const EdgeInsets.symmetric(horizontal: P6),
                   ),
-                ),
+                  const SizedBox(height: P3),
+                  QuizNextButton(_controller),
+                ],
+                if (_controller.isPromoFeaturesStep)
+                  PromoFeatures(wsMainController.myWS, onLater: _controller.next)
+                else if (_controller.isWhereWeGoStep) ...[
+                  H2(
+                    loc.onboarding_where_we_go_step_title,
+                    align: TextAlign.center,
+                    padding: const EdgeInsets.symmetric(horizontal: P6).copyWith(bottom: P2),
+                  ),
+                  MTCardButton(
+                    margin: const EdgeInsets.symmetric(horizontal: P4, vertical: P2),
+                    onTap: _startWithProject,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Row(children: [
+                          const ProjectsIcon(color: mainColor),
+                          const SizedBox(width: P2),
+                          Expanded(child: H3(loc.onboarding_start_with_project_title)),
+                          const ChevronIcon(),
+                        ]),
+                        const SizedBox(height: P2),
+                        BaseText(loc.onboarding_start_with_project_text, align: TextAlign.left, maxLines: 3),
+                      ],
+                    ),
+                  ),
+                  MTCardButton(
+                    margin: const EdgeInsets.symmetric(horizontal: P4, vertical: P2),
+                    onTap: _startWithTasks,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Row(children: [
+                          const DoneIcon(true, size: P6),
+                          const SizedBox(width: P2),
+                          Expanded(child: H3(loc.onboarding_start_with_tasks_title)),
+                          const ChevronIcon(),
+                        ]),
+                        const SizedBox(height: P2),
+                        BaseText(loc.onboarding_start_with_tasks_text, align: TextAlign.left, maxLines: 3),
+                      ],
+                    ),
+                  ),
+                ],
               ],
-            ],
+            ),
           ),
         ),
       );

a183366dafa4b9287d20f0d5930ea89e1f68896d
Alexandr Moroz <alexandrmoroz@gmail.com>
2024-07-28 18:49:36 +0300

    Нижняя панель инструментов в группе без задач

--- lib/L3_app/views/main/widgets/bottom_menu.dart
+++ lib/L3_app/views/main/widgets/bottom_menu.dart
@@ -1,7 +1,6 @@
 // Copyright (c) 2024. Alexandr Moroz
 
 import 'package:flutter/material.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
 
 import '../../../components/circle.dart';
 import '../../../components/colors.dart';
@@ -24,48 +23,46 @@ class BottomMenu extends StatelessWidget implements PreferredSizeWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Observer(
-      builder: (_) => Stack(
-        alignment: Alignment.bottomCenter,
-        clipBehavior: Clip.none,
-        children: [
-          MTAppBar(
-            isBottom: true,
+    return Stack(
+      alignment: Alignment.bottomCenter,
+      clipBehavior: Clip.none,
+      children: [
+        MTAppBar(
+          isBottom: true,
+          color: b2Color,
+          middle: Row(
+            children: [
+              Flexible(
+                child: MTListTile(
+                  middle: const InboxIcon(color: mainColor, size: P6),
+                  color: Colors.transparent,
+                  padding: _btnPadding,
+                  bottomDivider: false,
+                  onTap: () => router.goTaskView(tasksMainController.inbox!, direct: true),
+                ),
+              ),
+              const Spacer(),
+              Flexible(
+                child: MTListTile(
+                  middle: const ProjectsIcon(color: mainColor, size: P6),
+                  color: Colors.transparent,
+                  padding: _btnPadding,
+                  bottomDivider: false,
+                  onTap: router.goProjects,
+                ),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(
+          height: P12 + P7,
+          child: MTCircle(
+            size: P12 + P,
             color: b2Color,
-            middle: Row(
-              children: [
-                Flexible(
-                  child: MTListTile(
-                    middle: const InboxIcon(color: mainColor, size: P6),
-                    color: Colors.transparent,
-                    padding: _btnPadding,
-                    bottomDivider: false,
-                    onTap: () => router.goTaskView(tasksMainController.inbox!, direct: true),
-                  ),
-                ),
-                const Spacer(),
-                Flexible(
-                  child: MTListTile(
-                    middle: const ProjectsIcon(color: mainColor, size: P6),
-                    color: Colors.transparent,
-                    padding: _btnPadding,
-                    bottomDivider: false,
-                    onTap: router.goProjects,
-                  ),
-                ),
-              ],
-            ),
+            child: UnconstrainedBox(child: InboxAddTaskButton()),
           ),
-          const SizedBox(
-            height: P12 + P7,
-            child: MTCircle(
-              size: P12 + P,
-              color: b2Color,
-              child: UnconstrainedBox(child: InboxAddTaskButton()),
-            ),
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }

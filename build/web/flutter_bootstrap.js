abd983f1b445d45362dd4301cd653ccfb7ff890d
Alexandr Moroz <alexandrmoroz@gmail.com>
2024-11-07 16:42:41 +0300

    Bump version to 2.8.2816

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,7 +11,7 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "234036208",
+    serviceWorkerVersion: "4007463195",
   },
   onEntrypointLoaded: async function(engineInitializer) {
     // Firebase
